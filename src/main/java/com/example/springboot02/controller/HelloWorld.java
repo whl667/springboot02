4d7542db362a120891043882fe372a607116b1ee
wanghelong <645035958@qq.com>
2019-01-23 20:33:58 +0800

    Initial commit

---
+++ src/main/java/com/example/springboot02/controller/HelloWorld.java
@@ -0,0 +1,29 @@
+package com.example.springboot02.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/hello")
+    public String helloWorld(){
+        return "hello-world";
+    }
+    @RequestMapping("/hello2")
+    public String helloWorld02(){
+        return "hello-world02";
+    }
+    @RequestMapping("/hello3")
+    public String helloWorld03(){
+        System.out.println("测试");
+        return "hello-world03";
+    }
+    @RequestMapping("/hello4")
+    public String helloWorld04(){
+        System.out.println("生效了么?");
+        return "hello-world04";
+    }
+
+
+}
